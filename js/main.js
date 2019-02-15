411670f85c9ade84d304821256cb1193d3bbecf6
cristianboga <bogacristianrudolf@gmail.com>
2019-02-15 19:44:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
-
-const button = document.querySelector(".button");
 let palindromeString = document.getElementById("input");
 let resultContainer = document.querySelector('.result-message');
-const contentBox = document.querySelector('.content')
+const button = document.querySelector(".button");
+const contentBox = document.querySelector('.content');
+const isPalindrome = `<span class="green-result">It's a palindrome</span>`;
+const notPalindrome = `<span class="green-red">It's not a palindrome</span>`;
+const missingInputText = "^Input missing!";
 
-function palindrome(str){
 
-    if(str === "") {
-        resultContainer.innerHTML = "^Input missing!";
-        contentBox.style.boxShadow = "0px 2px 7px -1px red";
-    }else {
+function takeFromInput(){
+    palindromeString = document.getElementById("input").value;
+    palindrome(palindromeString)
+    document.getElementById("input").value = "";
+}
+
+function palindromeCheck(str){
     let abRegExp = /[^A-Za-z0-9]/g;
     let lowerString = str.toLowerCase().replace(abRegExp, '');
     let reverseString = lowerString.split('').reverse().join('');
     
-        if(reverseString === lowerString) {
-            resultContainer.innerHTML = `<span class="green-result">It's a palindrome</span>`;
-            contentBox.style.boxShadow = "0px 2px 7px -1px rgba(38, 194, 129, 1)";
-        }else {
-            resultContainer.innerHTML = `<span class="green-red">It's not a palindrome</span>`;
-            contentBox.style.boxShadow = "0px 2px 7px -1px red";
-        }
+    if(reverseString === lowerString) {
+        resultContainer.innerHTML = isPalindrome;
+        contentBox.style.boxShadow = "0px 2px 7px -1px rgba(38, 194, 129, 1)";
+    }else {
+        resultContainer.innerHTML = notPalindrome;
+        contentBox.style.boxShadow = "0px 2px 7px -1px red";
     }
+}
 
+function missingInput() {
+    resultContainer.innerHTML = missingInputText;
+    contentBox.style.boxShadow = "0px 2px 7px -1px red";
+}
+
+function palindrome(str){
+    if(str === "") {
+        missingInput();
+    }else {
+        palindromeCheck(palindromeString);
+    }
 }
 
 function removeStyle() {
@@ -30,10 +45,5 @@ function removeStyle() {
     contentBox.style.boxShadow = "0px 2px 7px -1px rgba(0,0,0,0.5)";
 }
 
-button.addEventListener('click', function(){
-    palindromeString = document.getElementById("input").value;
-    palindrome(palindromeString)
-    document.getElementById("input").value = "";
-});
-
-palindromeString.addEventListener('keydown',removeStyle);
+button.addEventListener('click', takeFromInput);
+palindromeString.addEventListener('keydown', removeStyle);
